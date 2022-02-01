4e706ff5c81f157edb66ade372a07a991d713d1f
l bates childs <labchilds@gmail.com>
2022-02-01 18:20:26 -0500

    add server.js and middleware

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const notes = require('./db/db.json');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// middle ware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
